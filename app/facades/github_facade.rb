d8a700deb841777babc1a1879f57b0c6fa133f6d
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-25 13:30:06 -0600

    Deleted pry's

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -1,14 +1,13 @@
 class GithubFacade
   def repo_creation(user)
     conn = Faraday.new("https://api.github.com")
-    respose = conn.get("user/repos", {visibility: 'all'}, {'Accept' => 'application/json', :Authorization => user.github_token})
+    response = conn.get("user/repos", {visibility: 'all'}, {'Accept' => 'application/json', :Authorization => user.github_token})
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
 
   def followers(user)
     conn = Faraday.new("https://api.github.com")
-    require "pry"; binding.pry
-    respose = conn.get("user/#{user.name}/followers", {visibility: 'all'}, {'Accept' => 'application/json', :Authorization => user.github_token})
+    response = conn.get("user/followers", {visibility: 'all'}, {'Accept' => 'application/json', :Authorization => user.github_token})
     parsed = JSON.parse(response.body, symbolize_names: true)
   end
 end
