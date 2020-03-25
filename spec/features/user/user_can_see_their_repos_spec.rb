3c8fd8be8df6fe3a534006a9e2c3344db60b5e6c
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-24 21:43:26 -0600

    For the first test I added another user to make sure it was only displaying the right repos. Added another test to make sure the github repo section wont show if the user doesnt have a token

--- spec/features/user/user_can_see_their_repos_spec.rb
+++ spec/features/user/user_can_see_their_repos_spec.rb
@@ -2,7 +2,6 @@ require "rails_helper"
 describe "User Dashboard: Github Repositories" do
   vcr_options = {:record => :new_episodes }
   it "as a user i should see my github repo section" do
-
     user1 = User.create!(email: "user1.gmail.com",
                       first_name: "user1",
                       last_name: "user1",
@@ -10,11 +9,36 @@ describe "User Dashboard: Github Repositories" do
                       role: 0,
                       github_token: ENV["GITHUB_API_KEY"])
 
+    user2 = User.create!(email: "user2.gmail.com",
+                      first_name: "user2",
+                      last_name: "user2",
+                      password: "hamburger2",
+                      role: 0,
+                      github_token: "5e6e5546ee4fdd541c2aa701699478dedfbd7cd1")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
-    github_repos = 5.times.map { Repo.new(name: "User Repositories", html_url: "www.github.com") }
-    allow(user1).to receive(:repos) {github_repos}
+
+    user1_repos = 5.times.map { Repo.new(name: "User 1 Repositories", html_url: "www.github.com") }
+    user2_repos = 5.times.map { Repo.new(name: "User 2 Repositories", html_url: "www.github.com") }
+
+    allow(user1).to receive(:repos) {user1_repos}
+    allow(user2).to receive(:repos) {user2_repos}
+
     visit '/dashboard'
-    expect(page).to have_link('User Repositories', href: 'www.github.com', count: 5)
+    expect(page).to have_link('User 1 Repositories', count: 5)
+    expect(page).to_not have_link('User 2 Repositories')
+  end
+
+  it "doesn't display github section when a user does not have a token" do
+    user1 = User.create!(email: "user1.gmail.com",
+                      first_name: "user1",
+                      last_name: "user1",
+                      password: "hamburger1",
+                      role: 0)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
+
+    visit '/dashboard'
+    expect(page).to_not have_content("Github Repositories")
   end
 end
