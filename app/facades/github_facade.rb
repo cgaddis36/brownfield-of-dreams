afbf3ca3f9bdb003cb15e7112406f77febf08e39
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-25 13:09:23 -0600

    Add follower method to github facade

--- app/facades/github_facade.rb
+++ app/facades/github_facade.rb
@@ -3,6 +3,12 @@ class GithubFacade
     conn = Faraday.new("https://api.github.com")
     respose = conn.get("user/repos", {visibility: 'all'}, {'Accept' => 'application/json', :Authorization => user.github_token})
     parsed = JSON.parse(response.body, symbolize_names: true)
-    
+  end
+
+  def followers(user)
+    conn = Faraday.new("https://api.github.com")
+    require "pry"; binding.pry
+    respose = conn.get("user/#{user.name}/followers", {visibility: 'all'}, {'Accept' => 'application/json', :Authorization => user.github_token})
+    parsed = JSON.parse(response.body, symbolize_names: true)
   end
 end
