ae7a6280d25b0952d904a5bdd30355bec5747da9
Chase.Gaddis <cgaddis36@gmail.com>
2020-03-27 11:12:14 -0500

    added logic to controller to remove any tutorials with classroom content if the user is not logged in yet

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -3,4 +3,12 @@ class TutorialsController < ApplicationController
     tutorial = Tutorial.find(params[:id])
     @facade = TutorialFacade.new(tutorial, params[:video_id])
   end
+
+  def index
+    if current_user
+      @tutorials = Tutorial.all
+    else
+      @tutorials = Tutorial.all.reject{|tutorial| tutorial.classroom? }
+    end
+  end
 end
