ea176e4dbede5775f08b23a05f4b9d28f7fca6f8
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-30 16:20:33 -0600

    Create a friendship model
    Co-Authored-By: Chase.Gaddis <cgaddis36@gmail.com>

---
+++ app/models/friendship.rb
@@ -0,0 +1,8 @@
+class Friendship < ApplicationRecord
+  belongs_to :user, foreign_key: "user_id", :class_name => :User
+  belongs_to :friend, foreign_key: "friend_id", :class_name => :User
+
+  def find_friend
+    User.where(id: self.friend_id).first
+  end
+end
