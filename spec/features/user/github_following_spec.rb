bd51bcdad3beead4cbe3bdffd2cc92fb35b3b08a
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-30 16:18:39 -0600

    Fix previous test to hit the API correctly

--- spec/features/user/github_following_spec.rb
+++ spec/features/user/github_following_spec.rb
@@ -1,37 +1,32 @@
 require "rails_helper"
 describe "User Dashboard: Github Following" do
-  vcr_options = {:record => :new_episodes }
-  it "users can see all users they are following and links to their page" do
-    user1 = User.create!(email: "user1@gmail.com",
-                      first_name: "user1",
-                      last_name: "user1",
-                      password: "hamburger1",
+  it "users can see all users they are following and links to their page", :vcr do
+    user1 = User.create!(email: 'user1@gmail.com',
+                      first_name: 'Meghan',
+                      last_name: 'Stovall',
+                      password: 'password1',
                       role: 0,
-                      github_token: ENV["GITHUB_API_KEY"])
-    user2 = User.create!(email: "user2@gmail.com",
-                      first_name: "user2",
-                      last_name: "user2",
-                      password: "hamburger2",
+                      github_token: "d3dce97f4fe7d42e913985756a13986d2e3db9e9",
+                      url: "https://github.com/meghanstovall")
+
+    user2 = User.create!(email: 'user2@gmail.com',
+                      first_name: 'Chase',
+                      last_name: 'Gaddis',
+                      password: 'password2',
                       role: 0,
-                      github_token: "5e6e5546ee4fdd541c2aa701699478dedfbd7cd1")
+                      github_token: "49217ac146e7db9618653e116848727e9780dacd",
+                      url: "https://github.com/cgaddis36")
 
     allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user1)
 
-    user1_repos = 5.times.map { Repo.new(name: "User 1 Repositories", html_url: "www.github.com") }
-    followers1 = 5.times.map { Follower.new(name: "Follower1", html_url: "www.github.com") }
-    following1 = 5.times.map { Following.new(name: "Following1", html_url: "www.github.com") }
-    following2 = 5.times.map { Following.new(name: "Following2", html_url: "www.github.com") }
-
-    allow(user1).to receive(:repos) {user1_repos}
-    allow(user1).to receive(:followers) {followers1}
-    allow(user1).to receive(:following) {following1}
-    allow(user2).to receive(:following) {following2}
-
     visit '/dashboard'
     expect(page).to have_content("Following")
+
     within "#following" do
-      expect(page).to have_link("Following1", count: 5)
-      expect(page).to_not have_link("Following2")
+      expect(page).to have_link("holmesm8")
+      expect(page).to have_link("Yetidancer")
+      expect(page).to have_link("kathleen-carroll")
+      expect(page).to_not have_link("cgaddis36")
     end
   end
 end
