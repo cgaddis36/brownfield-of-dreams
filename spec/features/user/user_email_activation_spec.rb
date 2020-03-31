367ddabba47159a7a14663298f5d119550d87c02
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-31 10:57:59 -0600

    Finish test for a new user registering and seeing flash messages

--- spec/features/user/user_email_activation_spec.rb
+++ spec/features/user/user_email_activation_spec.rb
@@ -3,17 +3,20 @@ require 'rails_helper'
 RSpec.describe "User Email Activation" do
   it "will get email after registering as a new user", :vcr do
     visit "/"
-    click_on "Register"
-    
 
+    click_on "Register"
+    expect(current_path).to eq("/register")
+
+    fill_in 'user[email]', with: "meghan.stovall@gmail.com"
+    fill_in 'user[first_name]', with: "Meghan"
+    fill_in 'user[last_name]', with: "Stovall"
+    fill_in 'user[password]', with: "password1"
+    fill_in 'user[password_confirmation]', with: "password1"
+
+    click_on "Create Account"
+    expect(current_path).to eq("/dashboard")
+
+    expect(page).to have_content("Logged in as Meghan Stovall")
+    expect(page).to have_content("This account has not yet been activated. Please check your email.")
   end
 end
-# Then I should be on "/register"
-# And when I fill in an email address (required)
-# And I fill in first name (required)
-# And I fill in last name (required)
-# And I fill in password and password confirmation (required)
-# And I click submit
-# Then I should be redirected to "/dashboard"
-# And I should see a message that says "Logged in as <SOME_NAME>"
-# And I should see a message that says "This account has not yet been activated. Please check your email."
