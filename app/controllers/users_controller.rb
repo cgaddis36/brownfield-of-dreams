0e836bdd3905f457a7cb1ffc6e9058ab65906960
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-31 10:59:17 -0600

    Add two flash messages, one to notify user to activate their account through their email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,9 +8,11 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
-    if @user.save
-      session[:user_id] = @user.id
+    user = User.create(user_params)
+    if user.save
+      session[:user_id] = user.id
+      flash[:notice] = "Logged in as #{user.first_name} #{user.last_name}"
+      flash[:error] = "This account has not yet been activated. Please check your email."
       redirect_to dashboard_path
     else
       flash[:error] = 'Username already exists'
