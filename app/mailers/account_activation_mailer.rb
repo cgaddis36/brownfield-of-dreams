3be80472522799036cca3c38769187a6d05d527b
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-31 11:56:05 -0600

    Create an Email Activation Mailer and the cooresponding views

---
+++ app/mailers/account_activation_mailer.rb
@@ -0,0 +1,8 @@
+class AccountActivationMailer < ApplicationMailer
+  def inform(user)
+    @user = user
+    @token = SecureRandom.hex(10).to_s
+
+    mail(to: user.email, subject: "Activate Account")
+  end
+end
