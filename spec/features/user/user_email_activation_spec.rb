d1aa5c5e5875a4cdf41999f2665a3c110904a37c
Meghan Stovall <meghanstovall15@augustana.edu>
2020-03-31 11:54:56 -0600

    Add a second test to check when an email has been confirmed, the user's status changes

--- spec/features/user/user_email_activation_spec.rb
+++ spec/features/user/user_email_activation_spec.rb
@@ -18,5 +18,28 @@ RSpec.describe "User Email Activation" do
 
     expect(page).to have_content("Logged in as Meghan Stovall")
     expect(page).to have_content("This account has not yet been activated. Please check your email.")
+    expect(page).to have_content("Status: Inactive")
+  end
+
+  it "user is sent email after registering" do
+    visit "/"
+
+    click_on "Register"
+    expect(current_path).to eq("/register")
+
+    fill_in 'user[email]', with: "meghan.stovall@gmail.com"
+    fill_in 'user[first_name]', with: "Meghan"
+    fill_in 'user[last_name]', with: "Stovall"
+    fill_in 'user[password]', with: "password1"
+    fill_in 'user[password_confirmation]', with: "password1"
+
+    click_on "Create Account"
+    expect(current_path).to eq("/dashboard")
+
+    User.last.update_column(:email_confirm, true)
+
+    page.refresh
+
+    expect(page).to have_content('Status: Active')
   end
 end
