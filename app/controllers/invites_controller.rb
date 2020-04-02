8ba94709e6a213817a747c3da17af5e6e264fd45
Chase.Gaddis <cgaddis36@gmail.com>
2020-04-02 08:50:11 -0500

    line length

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -10,7 +10,8 @@ class InvitesController < ApplicationController
       InviteMailer.invite(user, current_user).deliver_now
       flash[:notice] = 'Successfully sent invite!'
     else
-      flash[:error] = "The Github user you selected doesn't have an email address associated with their account."
+      flash[:error] =
+        "The Github user you selected doesn't have an email address associated with their account."
     end
     redirect_to dashboard_path
   end
