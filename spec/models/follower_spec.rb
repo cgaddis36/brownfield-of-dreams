1f9d87a3d8d6a2f22f7653c39c75405d9560f2fc
Meghan Stovall <meghanstovall15@augustana.edu>
2020-04-02 12:08:50 -0600

    Test the database method in the follower poro

--- spec/models/follower_spec.rb
+++ spec/models/follower_spec.rb
@@ -1,23 +1,22 @@
 require 'rails_helper'
 
-RSpec.describe Tutorial, type: :model do
-  describe "validations" do
-    it {should validate_presence_of :title}
-    it {should validate_presence_of :description}
-    it {should validate_presence_of :thumbnail}
-  end
-
-
-  describe "relationships" do
-    it { should have_many :videos }
-  end
-
+RSpec.describe Follower, type: :model do
   describe "methods" do
-    it "#find_friend" do
-      user1 = User.create(email: 'user1@email.com', password: 'password1', first_name:'Jim', role: 0, email_confirm: true)
-      user2 = User.create(email: 'user2@email.com', password: 'password1', first_name:'Jim', role: 0, email_confirm: true)
-      friendship = Friendship.create!(user_id: user1.id, friend_id: user2.id)
-      expect(friendship.find_friend).to eq(user2)
+    it "#database?" do
+      user = User.create!(email: 'meghanstovall@gmail.com',
+                  first_name: 'Meghan',
+                  last_name: 'Stovall',
+                  password: 'Turing',
+                  role: 1,
+                  github_token: ENV['token'],
+                  url: 'https://github.com/meghanstovall',
+                  email_confirm: true)
+
+      follower1 = Follower.new({name: "Meghan", html_url: 'https://github.com/meghanstovall'})
+      expect(follower1.database?).to eq(true)
+
+      follower2 = Follower.new({name: "Chase", html_url: 'https://github.com/cgaddis36'})
+      expect(follower2.database?).to eq(false)
     end
   end
 end
